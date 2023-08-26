d96e8f353142f2f762c9cdf2724399d41c2268e8
Xavier Armitage <xtarmitage@gmail.com>
2023-08-26 21:41:18 +1000

    added create summaries functionality

--- project/app/main.py
+++ project/app/main.py
@@ -5,7 +5,7 @@ import logging
 from fastapi import FastAPI, Depends
 from tortoise.contrib.fastapi import register_tortoise
 
-from app.api import ping
+from app.api import ping, summaries
 from app.db import init_db
 
 log = logging.getLogger("uvicorn")
@@ -14,6 +14,9 @@ log = logging.getLogger("uvicorn")
 def create_application() -> FastAPI:
     application = FastAPI()
     application.include_router(ping.router)
+    application.include_router(
+        summaries.router, prefix="/summaries", tags=["summaries"]
+    )
     return application
 
 
--- project/app/models/pydantic.py
+++ project/app/models/pydantic.py
@@ -5,3 +5,7 @@ from pydantic import BaseModel
 
 class SummaryPayloadSchema(BaseModel):
     url: str
+
+
+class SummaryResponseSchema(SummaryPayloadSchema):
+    id: int
