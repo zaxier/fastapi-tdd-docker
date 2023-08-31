a499710dd0d7778ede0bb44d1d9343924ca1b5df
Xavier Armitage <xtarmitage@gmail.com>
2023-08-31 22:24:58 +1000

    black fix

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -2,8 +2,7 @@
 
 from typing import Union
 
-from app.models.pydantic import (SummaryPayloadSchema,
-                                 SummaryUpdatePayloadSchema)
+from app.models.pydantic import SummaryPayloadSchema, SummaryUpdatePayloadSchema
 from app.models.tortoise import TextSummary
 
 
