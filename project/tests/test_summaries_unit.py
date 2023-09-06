636384b3c96fd660bc49efd5c6af506f9decae8e
Xavier Armitage <xtarmitage@gmail.com>
2023-09-06 14:14:28 +1000

    removed test

--- project/tests/test_summaries_unit.py
+++ project/tests/test_summaries_unit.py
@@ -218,12 +218,3 @@ def test_update_summary_invalid(
     response = test_app.put(f"/summaries/{summary_id}/", data=json.dumps(payload))
     assert response.status_code == status_code
     assert response.json()["detail"] == detail
-
-
-def test_update_summary_invalid_url(test_app):
-    response = test_app.put(
-        f"/summaries/1/",
-        data=json.dumps({"url": "invalid://url", "summary": "updated!"}),
-    )
-    assert response.status_code == 422
-    assert response.json()["detail"][0]["msg"] == "URL scheme not permitted"
